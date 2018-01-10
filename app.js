ec4fe5b399b842f994679a95a4994a031e0f5182
Illya Yanchuk <illyayanchuk@Laurens-MacBook-Air.local>
2018-01-09 18:44:44 -0800

    got working client-server

--- app.js
+++ app.js
@@ -2,11 +2,34 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
+
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
+
 const app = express();
 app.use(index);
+
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting! // this initializes a new instance of socketIo
 // by passing it in the server object. This "wires up" the server to Socket.IO
-const getApiAndEmit = "TODO"
+io.on("connection", socket => {
+  console.log("New client connected"), setInterval(
+    () => getApiAndEmit(socket),
+    10000
+  );
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+const getApiAndEmit = async socket => { // need to learn what this is doing, i think its axios
+  try {
+    const res = await axios.get(
+    	// ****** NEED TO REFACTOR API KEY TO A .GITIGNORED CONFIG FILE! ******
+      "https://api.darksky.net/forecast/_API_KEY_GOES_HERE_/43.7695,11.2558"
+    );
+    socket.emit("FromAPI", res.data.currently.temperature);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
